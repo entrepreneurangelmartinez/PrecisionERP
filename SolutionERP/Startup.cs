d46cb03c0325f2beaea55970c10a7d3d9dee98bf
Angel Martínez Frías <angeljesusmartinezfrias@hotmail.com>
2015-10-13 00:53:44 -0500

    Esquema principal

---
+++ SolutionERP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SolutionERP.Startup))]
+namespace SolutionERP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
